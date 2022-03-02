5a8885eca2d1cc796043f358733a934fb799f461
ManoelRIos <manoel.rios@cfc.org.br>
2022-03-02 18:00:27 -0300

    Adicionando component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
+import { Route } from 'react-router-dom'
+import { Home } from './components/Home'
+
 function App() {
 
-  function formatName(user){
-    return user.firstName + ' ' + user.lastName;
-  }
-
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-  };
-
   return (
-    <div className="App">
-      <h1>Hello, {formatName(user)}! </h1>
-    </div>
+    <Route path="/" exact component={Home}/>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+export function Header(){
+
+  return(
+
+    <div className="header">
+      <div className="logo">
+        <span>React</span><span>.js</span>
+      </div>
+      <nav>
+        <li><a class="title" href="#home">Início</a></li>
+        <li><a class="title" href="#about">Sobre</a></li>
+        <li><a class="title" href="#services">Serviço</a></li>
+        <li><a class="title" href="#testimonials">Postagens</a></li>
+        <li><a class="title" href="#contact">Contato</a></li>
+      </nav>
+    </div>
+
+
+  )
+
+}
