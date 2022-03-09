63a6b3f7552bd4f5a38555675d5682c9f6a0d3ff
ManoelRIos <manoel.rios@cfc.org.br>
2022-03-09 17:30:49 -0300

    components and props

--- src/index.js
+++ src/index.js
@@ -2,19 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function tick(){
-  const element = (
-    <div>
-      <h1>Hello world</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  )
-  
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}    
+      />
+    )
+  }
 }
 
-setInterval(tick, 1000)
+function Avatar(props){
+  return(
+    <img src={props.user.avatarUrl}
+      alt={props.user.name} />
+  )
+}
 
+function userInfo(props){
+  return(
+    <Avatar user={props.user}/>
+    {props.user.name}
+  )
+}
+
+function Comment(props){
+
+  return(
+    <div className="comment">
+      <UserInfo user={props.author}/>
+      {props.text}
+      {formatDate(props.date)}
+    </div>
+  )
+}
+
+const comment = {
+  date: new Date(),
+  text: "Hello world!",
+  author:{
+    name:'Hello kit',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
